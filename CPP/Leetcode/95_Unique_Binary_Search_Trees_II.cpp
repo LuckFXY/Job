686a6c1428356a93159407d7c02ef0d0a244f404
lab_ubuntu <luck_fengxiaoyu@163.com>
2018-08-15 11:41:13 +0800

    ubuntu submit

---
+++ CPP/Leetcode/95_Unique_Binary_Search_Trees_II.cpp
@@ -0,0 +1,45 @@
+//
+// Created by Administrator on 2018/8/14.
+//
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+}TreeNode;
+inline void swap(vector<int>& arr, int i, int j){
+    int tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
+}
+inline void vprint(vector<int> arr){
+    for(auto it = arr.begin(); it != arr.end(); it++)
+        cout<<(*it)<<' ';
+    cout<<endl;
+}
+vector<TreeNode*> generateTrees(int n) {
+    vector<int> unique_bst;
+    for(int i=1; i<=n; i++)
+        unique_bst.emplace_back(i);
+    for(int i=n+1; i<2*n; i++)
+        unique_bst.emplace_back(0);
+
+    vector<vector<int>> result;
+    result.emplace_back(unique_bst);
+    int idx = n;
+    for(int i=n-1; i>0; i--){
+        idx -= 1;
+        for(int j=idx; j<2*n; j++){
+            if(unique_bst[j+1] == 0 && unique_bst[j-1] != 0) {
+                swap(unique_bst, j, j + 1);
+                vprint(unique_bst);
+                result.emplace_back(unique_bst);
+            }
+        }
+    }
+}
---
+++ CPP/Leetcode/bag_problem_9.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Administrator on 2018/8/14.
+//
+
+
+void zero_one_bag(){
+
+}
