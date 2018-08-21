99a5435f0833152a7d86b785d11edec09c7b6ba8
lab_ubuntu <luck_fengxiaoyu@163.com>
2018-08-21 13:55:56 +0800

    good luck

--- CPP/Leetcode/main.cpp
+++ CPP/Leetcode/main.cpp
@@ -1,10 +1,4 @@
 
-#include <iostream>
-#include <vector>
-#include <map>
-#include <set>
-using namespace std;
-typedef pair<int, int> PAIR;
 //int walk(map<int, PAIR> graph, int start){
 //    int total = 0;
 //    vector<int> stack;
@@ -36,32 +30,93 @@ typedef pair<int, int> PAIR;
 //    return 0;
 //}
 
-#include <cstdio>
-#include <algorithm>
-typedef pair<int, int> PAIR;
-ostream& operator << (ostream& out, const PAIR src){
-    out<<src.first<<' '<<src.second;
-    return out;
-}
-bool comp(PAIR a, PAIR b){
-    return a.first < b.first;
-}
-#include <string>
-#include <algoritm>
-int main(){
-    vector<int> data;
-    int a[] = {1,2,3,4,5,};
-    data.assign(a, a+3);
-    data.insert(data.begin(), 0);
-    data.erase(data.begin());
+//#include <iostream>
+//#include <stdio.h>
+//#include <map>
+//#include <vector>
+//#include <string>
+//using namespace std;
+//int main(){
+//
+//    map<int , map<int, int>> graph;
+//    vector<vector<int>> data={
+//            {0,1},
+//            {0,5},
+//            {1,2},
+//            {1,3},
+//            {2,3},
+//            {3,4},
+//            {3,5}
+//    };
+//    int weight[6] = {2,8,3,5,6,1};
+//    for(int i=0; i<6; i++){
+//        int start = data[i][0], end = data[i][1];
+//        if(graph.find(start) == graph.end()){
+//            graph.insert(
+//                    make_pair(
+//                            start, map<int, int>()
+//                    ));
+//            graph[start][end] = weight[end];
+//        }
+//        else{
+//            map<int, int>& inner = graph[start];
+//            inner.insert(make_pair(end, weight[end]));
+//        }
+//    }
+//
+//    for(auto it = graph.begin(); it != graph.end(); it++){
+//        printf("%d : ", (*it).first);
+//        for(auto jt = (*it).second.begin(); jt != (*it).second.end(); jt++ ){
+//            printf("(%d %d) ",(*jt).first, (*jt).second);
+//        }
+//        cout<<endl;
+//    }
+//    return 0;
+//}
+#include <stdio.h>
+#include <iostream>
+using namespace std;
 
-    for(auto it = data.begin(); it != data.end(); it++){
-        cout<< *it <<endl;
+
+class Person {
+public:
+    Person(const string& name, int age) : m_name(name), m_age(age) {}
+    int a = 123;
+    void ShowInfo() {
+        cout << "姓名：" << m_name << endl;
+        cout << "年龄：" << m_age << endl;
     }
 
-    //sort(data.begin(), data.end());
-//    for(auto it = data.begin(); it != data.end(); it++){
-//        cout<< *it <<endl;
-//    }
-    return 0;
+protected:
+    string  m_name;     //姓名
+
+private:
+    int     m_age;      //年龄
+};
+
+class Teacher : private Person
+{
+public:
+    Teacher(const string& name, int age, const string& title)
+            : Person(name, age), m_title(title)
+    {
+    }
+
+    void ShowTeacherInfo()
+    {
+        ShowInfo();                             //正确，public属性子类可见
+        cout << "姓名：" << m_name << endl;        //正确，protected属性子类可见
+        //cout << "年龄：" << m_age << endl;     //错误，private属性子类不可见
+
+        cout << "职称：" << m_title << endl;   //正确，本类中可见自己的所有成员
+    }
+
+private:
+    string  m_title;        //职称
+};
+int main(){
+
+    for(int i=1; i<15; i++){
+        printf("%d mod %d = %d\n", i, 15, i% 15);
+    }
 }
